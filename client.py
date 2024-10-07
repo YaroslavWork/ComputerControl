dce0645cb94ecce87d92377dbcfccf688341f441
Yaroslav Zahorodnyi <zahorodnyi.yaroslav.work@gmail.com>
2024-10-07 11:30:36 +0200

    Working server-client tcp conn with controling computer

--- client.py
+++ client.py
@@ -1,15 +1,22 @@
 import threading
 import socket
+import netifaces
 
-HOST = input("Input a host --> ")
-PORT = 49474
+HOST, PORT = input("Input a address --> ").split(":")
+cookies = None
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object (AF_INET: address family, SOCK_STREAM: socket type)
 
 try:
-    s.connect((HOST, PORT))  # Connect to the server
+    s.connect((HOST, int(PORT)))  # Connect to the server
 except ConnectionRefusedError:
     print("Connection is not find")
+    exit()
+except ValueError:
+    print("Input a correct port")
+    exit()
+except socket.gaierror:
+    print("Input a correct address")
     exit()
 
 while True:
@@ -21,12 +28,18 @@ while True:
             s.send(nickname.encode("utf-8"))
         elif attr[0] == "COM":
             command = input("Input a command (type 'HELP' to show all commands) --> ")
-            s.send(command.encode("utf-8"))
+            s.send(f"{cookies} {command}".encode("utf-8"))
         elif attr[0] == "DIS":
             print("Quiting...")
             break
         elif attr[0] == "MES":
             print(" ".join(attr[1:]))
+        elif attr[0] == "COOK":
+            cookies = attr[1]
+            print("Cookies set")
+            s.send("SUCCESS".encode("utf-8"))
+            command = input("Input a command (type 'HELP' to show all commands) --> ")
+            s.send(f"{cookies} {command}".encode("utf-8"))
     except ConnectionResetError:
         print("Server is not responding")
         break
--- server.py
+++ server.py
@@ -2,25 +2,48 @@ import threading
 import socket
 import webbrowser
 from hashlib import sha256
+import pyautogui
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import os
 
-HOST = open("host.txt", "r").read()
-PORT = 49474
+HOST, PORT = open("host.txt", "r").read().split(":")
 
 # password from password.txt
 PASSWORD = open("password.txt", "r").read()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object (AF_INET: address family, SOCK_STREAM: socket type)
 
-server.bind((HOST, PORT))  # Bind the socket to the address and port
+server.bind((HOST, int(PORT)))  # Bind the socket to the address and port
 server.listen()  # Listen for incoming connections
 
 clients = []
+cookies = []
 
 def go_to_page(url):
     webbrowser.open(url)
 
-def set_volume(volume: float) -> str:
-    return f""
+def set_global_volume(level) -> str:
+    # Get the default audio device
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+
+    # Cast the interface to a pointer to IAudioEndpointVolume
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+    # Set the volume level (0.0 to 1.0)
+    volume.SetMasterVolumeLevelScalar(level, None)
+    return "Volume is set"
+
+def move_offset(x: int, y: int) -> str:
+    pyautogui.move(x, y)
+    return "Mouse moved"
+
+def click() -> str:
+    pyautogui.click()
+    return "Mouse clicked"
 
 HELP_STR = '''
 Commands:
@@ -28,10 +51,9 @@ Commands:
 'URL [-url]' - open the URL in the browser
     Options:
         -url     url adress
-'VOL [-volume] [-name]' - set the volume of the application
+'VOL [-volume]' - set the volume of the application
     Options:
         -volume  volume level
-        -name    name of the application
 '''
 
 def handle(client):
@@ -40,41 +62,67 @@ def handle(client):
             command = client.recv(1024)
             # Split the message by spacing
             attributes = command.decode("utf-8").split(" ")
-            if attributes[0] == "HELP":
-                client.send(f"MES {HELP_STR}".encode("utf-8"))
-            elif attributes[0] == "URL":
-                go_to_page(attributes[1])
-                client.send("MES URL is opened!".encode("utf-8"))
-            elif attributes[0] == "VOL":
-                mes_ret = set_volume(attributes[1], attributes[2])
-                client.send(f"MES {mes_ret}".encode("utf-8"))
-            client.send("COM".encode("utf-8"))
+            if attributes[0] in cookies:
+                if attributes[1] == "HELP":
+                    client.send(f"MES {HELP_STR}".encode("utf-8"))
+                elif attributes[1] == "URL":
+                    go_to_page(attributes[2])
+                    client.send("MES URL is opened!".encode("utf-8"))
+                elif attributes[1] == "VOL":
+                    mes_ret = set_global_volume(float(attributes[2]))
+                    client.send(f"MES {mes_ret}".encode("utf-8"))
+                elif attributes[1] == "MOV":
+                    mes_ret = move_offset(int(attributes[2]), int(attributes[3]))
+                    client.send(f"MES {mes_ret}".encode("utf-8"))
+                elif attributes[1] == "CLK":
+                    mes_ret = click()
+                    client.send(f"MES {mes_ret}".encode("utf-8"))
+                elif attributes[1] == "LOCK":
+                    os.system("rundll32.exe user32.dll,LockWorkStation")
+                elif attributes[1] == "SHUT":
+                    if len(attributes) > 1:
+                        os.system(f"shutdown /s /t {attributes[2]}")
+                    else:
+                        os.system("shutdown /s /t 1")
+                client.send("COM".encode("utf-8"))
         except:
             clients.remove(client)
+            cookies.remove(client)
             client.close()
+            print(f"Client disconnected: {client}")
             break
+
+def auth(client, sha_password: str) -> None:
+    while True:
+        client.send("PAS".encode("utf-8"))
+        password = client.recv(1024).decode("utf-8")
+        if sha256(password.encode("utf-8")).hexdigest() != sha_password:
+            client.send("DIS".encode("utf-8"))
+            client.close()
+            print(f"Client disconnected: {client}")
+            continue
+        clients.append(client)
+        cookie = sha256(str(os.urandom(1024)).encode("utf-8")).hexdigest()
+        cookies.append(cookie)
+
+        print(f"Client authenticated: {client}")
+        client.send("MES Connected to the server!".encode("utf-8"))
+        client.send(f"COOK {cookie}".encode("utf-8"))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+        break
 
 def receive():
     while True:
         client, address = server.accept()
-        print(f"Connected with{address}")
+        print(f"This address connecting: {address}")
 
-        client.send("PAS".encode("utf-8"))
-        password = client.recv(1024).decode("utf-8")
-        if sha256(password.encode("utf-8")).hexdigest() != PASSWORD:
-            client.send("DIS".encode("utf-8"))
-            client.close()
-            continue
-
-        clients.append(client)
-
-        print(f"Client connected: {client}")
-        client.send("Connected to the server!".encode("utf-8"))
-        client.send("COM".encode("utf-8"))
-
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target=auth, args=(client, PASSWORD))
         thread.start()
 
-print("Server is listening...")
+
+print(f"Server is listening on {HOST}:{PORT}")
 if __name__ == "__main__":
     receive()
+    print(sha256("123".encode("utf-8")).hexdigest())
